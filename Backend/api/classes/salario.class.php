4d31c6c8a34c89732c9dd3a593a5a4e6034ebca3
Nicolas Moreno Duran <morenodurann@gmail.com>
2022-01-28 23:32:56 -0600

    asd

--- Backend/api/classes/salario.class.php
+++ Backend/api/classes/salario.class.php
@@ -8,59 +8,63 @@ class Salario
 {
     private $_dbh = null;
 
-    function __construct()
+    public function __construct()
     {
         $this->_dbh = new DataBase();
-        
     }
 
-    public function obtenerSalarios()
+    public function obtenerSalarios($limit, $skip)
     {
-        $result = $this->_dbh->consultar('SELECT * FROM Salario', []);
+        echo $limit;
+        $result = $this->_dbh->consultar('SELECT * FROM Salario limit '.$skip.', '.$limit, []);
 
-        if ($result['ok'])
+        if ($result['ok']) {
             echo  json_encode(array(
                 "ok" => $result['ok'],
                 "result" => $result['result']
             ));
-        else
+        } else {
             echo  json_encode(array(
                 "ok" => $result['ok'],
                 "message" => 'No hay salarios',
+                "error" => $result['error'],
             ));
+        }
     }
     
     public function insertarSalario($nombre, $cantidad)
     {
         $result = $this->_dbh->insertar_actuallzar_eliminar('INSERT INTO Salario VALUES (0, ?, ?)', [$nombre, $cantidad]);
 
-        if ($result['ok'])
+        if ($result['ok']) {
             echo  json_encode(array(
                 "ok" => $result['ok'],
                 "message" => 'Salario guardado'
             ));
-        else
+        } else {
             echo  json_encode(array(
                 "ok" => $result['ok'],
                 "message" => 'Error al guardar el salario',
                 "error" => $result['error']
             ));
+        }
     }
 
     public function actualizarSalario($id, $nombre, $cantidad)
     {
         $result = $this->_dbh->insertar_actuallzar_eliminar('UPDATE Salario SET nombre = ?, cantidad = ? WHERE id = ?', [$nombre, $cantidad, $id]);
 
-        if ($result['ok'])
+        if ($result['ok']) {
             echo  json_encode(array(
                 "ok" => $result['ok'],
                 "message" => 'Salario actualizado'
             ));
-        else
+        } else {
             echo  json_encode(array(
                 "ok" => $result['ok'],
                 "message" => 'Error al actualizar el salario',
                 "error" => $result['error']
             ));
+        }
     }
 }
--- Backend/api/endpoints/salario.php
+++ Backend/api/endpoints/salario.php
@@ -10,7 +10,11 @@ $salario = new Salario();
 
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
-        $salario->obtenerSalarios();
+        echo $_GET['limit'] .'<br>';
+        $limit =  $_GET['limit'] | 10;
+        echo $limit.'<br>';
+        $skip  =  $_GET['skip'] | 0;
+        $salario->obtenerSalarios($limit, $skip);
         break;
 
     case 'POST':
@@ -19,7 +23,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         break;
     case 'PUT':
         $_PUT = json_decode(file_get_contents('php://input'), true);
-        $result = $salario->actualizarSalario($_PUT['id'],$_PUT['nombre'], $_PUT['cantidad']);
+        $result = $salario->actualizarSalario($_PUT['id'], $_PUT['nombre'], $_PUT['cantidad']);
         break;
     case 'DELETE':
         echo 'Método DELETE';
